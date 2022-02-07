c8d653e0fa7010b15b06193f1a4c9f0d371af47f
Nilotpal <36688839+nilotpalutkalit@users.noreply.github.com>
2022-02-08 01:50:23 +0530

    Grouping by device name

---
+++ PhotoGroup/AttributeParser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace PhotoGroup
+{
+    public class AttributeParser
+    {
+        public AttributeParser()
+        {
+        }
+
+        public Dictionary<string, string> GetAttribute(string fileName)
+        {
+            Dictionary<string, string> attributes = new Dictionary<string, string>();
+            ProcessStartInfo startInfo = new ProcessStartInfo()
+            {
+                FileName = "mdls",
+                Arguments = "\"" + string.Format(fileName) + "\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+            Process process = new Process()
+            {
+                StartInfo = startInfo,
+            };
+            process.Start();
+            var result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            Regex regex = new Regex(@"([_k]+.*):?=(.*)");
+            var match = regex.Matches(result);
+            foreach (Match eachMatch in match)
+            {
+                attributes.Add(eachMatch.Groups[1].ToString().Trim(), eachMatch.Groups[2].ToString().Trim('\"',' '));
+            }
+
+            return attributes;
+        }
+    }
+}
---
+++ PhotoGroup/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace PhotoGroup
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            AttributeParser attributeParser = new AttributeParser();
+            Console.WriteLine("Hello World!");
+            try
+            {
+                string PhotoPath = "/Volumes/Fast SSD/MacOSXPictures_All";
+
+                HashSet<string> allPhotos = new HashSet<string>(Directory.EnumerateFiles(PhotoPath));
+
+                foreach (var fileName in allPhotos)
+                {
+                    if(fileName.StartsWith("."))
+                    {
+                        continue;
+                    }
+
+                    var attributes = attributeParser.GetAttribute(fileName);
+                    if(attributes.ContainsKey("kMDItemAcquisitionModel") && attributes.ContainsKey("kMDItemAcquisitionMake"))
+                    {
+                        var folderName = Path.Combine(Path.GetDirectoryName(fileName), attributes["kMDItemAcquisitionMake"] + " " + attributes["kMDItemAcquisitionModel"]);
+                        if(!Directory.Exists(folderName))
+                        {
+                            Directory.CreateDirectory(folderName);
+                        }
+
+                        var destinationFileName = Path.Combine(folderName, Path.GetFileName(fileName));
+                        File.Move(fileName, destinationFileName);
+                    }
+
+
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (PathTooLongException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+    }
+}
