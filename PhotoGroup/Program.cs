a2327d69782de8ca93d25f3df9fb184d9b16ee85
Nilotpal <36688839+nilotpalutkalit@users.noreply.github.com>
2022-02-08 12:20:09 +0530

    Group by resolution, device, extension and others

--- PhotoGroup/Program.cs
+++ PhotoGroup/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace PhotoGroup
 {
@@ -25,10 +26,44 @@ namespace PhotoGroup
                     }
 
                     var attributes = attributeParser.GetAttribute(fileName);
-                    if(attributes.ContainsKey("kMDItemAcquisitionModel") && attributes.ContainsKey("kMDItemAcquisitionMake"))
+                    var extension = Path.GetExtension(fileName).TrimStart('.');  
+                    if (attributes.ContainsKey("kMDItemAcquisitionModel") && attributes.ContainsKey("kMDItemAcquisitionMake"))
                     {
                         var folderName = Path.Combine(Path.GetDirectoryName(fileName), attributes["kMDItemAcquisitionMake"] + " " + attributes["kMDItemAcquisitionModel"]);
                         if(!Directory.Exists(folderName))
+                        {
+                            Directory.CreateDirectory(folderName);
+                        }
+                        
+                        var destinationFileName = Path.Combine(folderName, Path.GetFileName(fileName));
+                        File.Move(fileName, destinationFileName);
+                    }
+                    else if(!extension.Equals("jpeg") && !extension.Equals("png") &&
+                        !extension.Equals("heic") && !extension.Equals("DS_Store"))
+                    {
+
+                        var folderName = Path.Combine(Path.GetDirectoryName(fileName), Path.GetExtension(fileName).TrimStart('.'));
+                        if (!Directory.Exists(folderName))
+                        {
+                            Directory.CreateDirectory(folderName);
+                        }
+
+                        var destinationFileName = Path.Combine(folderName, Path.GetFileName(fileName));
+                        File.Move(fileName, destinationFileName);
+
+                    }
+                    else if(attributes.ContainsKey("kMDItemPixelHeight") && attributes.ContainsKey("kMDItemPixelWidth"))
+                    {
+                        var width = int.Parse(attributes["kMDItemPixelWidth"]);
+                        var height = int.Parse(attributes["kMDItemPixelHeight"]);
+                        var dimensionMax = (width > height) ? width : height;
+                        if(dimensionMax<720)
+                        {
+                            continue;
+                        }
+                        dimensionMax = dimensionMax - (dimensionMax % 100);
+                        var folderName = Path.Combine(Path.GetDirectoryName(fileName), dimensionMax.ToString());
+                        if (!Directory.Exists(folderName))
                         {
                             Directory.CreateDirectory(folderName);
                         }
